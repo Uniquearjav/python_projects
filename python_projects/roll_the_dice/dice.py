9c68ffed09446f368f829314c59fd4b023b776a2
Arjav Choudhary <92720814+Uniquearjav@users.noreply.github.com>
2024-04-12 21:37:59 +0530

    Delete python_projects directory

--- python_projects/roll_the_dice/dice.py
+++
@@ -1,9 +0,0 @@
-import random
-
-while True:
-    print('Welcome to the dice rolling simulator!')
-    input_dice = input('Press enter to roll the dice or type "exit" to quit.')
-    if input_dice == 'exit':
-        break
-    else:
-        print(random.randint(1, 6))
